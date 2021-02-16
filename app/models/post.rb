5b699ca99a75470b8545a1f93e26d8238e9ad00e
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-02-16 14:24:31 -0600

    create associations in models for Posts and Users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
   validates :link, presence: true, length: { minimum: 6}, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
+
     validates :username, presence: true, length: { minimum: 2}, uniqueness: true
 end
